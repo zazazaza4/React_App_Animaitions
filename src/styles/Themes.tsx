d479d2750da29343dbafb5b698a0dd13cdf05ceb
Yaroslav <6715414@gmail.com>
2023-02-18 10:49:27 +0200

    added type dark theme

--- src/styles/Themes.tsx
+++ src/styles/Themes.tsx
@@ -9,7 +9,7 @@ export const dark = {
   fontxs: "0.75em",
   fontsm: "0.875em",
   fontmd: "1em",
-  fontls: "1.25em",
+  fontlg: "1.25em",
   fontxl: "2em",
   fontxxl: "3em",
   fontxxxl: "5em",
@@ -17,3 +17,5 @@ export const dark = {
 
   navHeight: "5rem",
 };
+
+export type darkThemeType = typeof dark;
