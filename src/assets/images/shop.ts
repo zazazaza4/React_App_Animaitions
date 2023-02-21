3fec7abd0e1cd7b11bef414c43cddb72eb832ed9
Yaroslav <6715414@gmail.com>
2023-02-21 21:41:13 +0200

    added array of images for shop section

---
+++ src/assets/images/shop.ts
@@ -0,0 +1,53 @@
+import img1 from "./3.webp";
+import img2 from "./4.webp";
+import img3 from "./5.webp";
+import img4 from "./6.webp";
+import img5 from "./7.webp";
+import img6 from "./8.webp";
+import img7 from "./9.webp";
+import img8 from "./10.webp";
+import img9 from "./11.webp";
+import img10 from "./12.webp";
+
+export const productArray = [
+  {
+    img: img1,
+    title: "Man Basics",
+  },
+  {
+    img: img2,
+    title: "Tops",
+  },
+  {
+    img: img3,
+    title: "Sweatshirts",
+  },
+  {
+    img: img4,
+    title: "Ethic Wear",
+  },
+  {
+    img: img5,
+    title: "Blazers",
+  },
+  {
+    img: img6,
+    title: "Swits",
+  },
+  {
+    img: img7,
+    title: "Antiques",
+  },
+  {
+    img: img8,
+    title: "Jewellery",
+  },
+  {
+    img: img9,
+    title: "Watches",
+  },
+  {
+    img: img10,
+    title: "Special Edition",
+  },
+];
