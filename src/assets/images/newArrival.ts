12b249b97ded6259dec31c73c3089ef94c524ee8
Yaroslav <6715414@gmail.com>
2023-02-21 21:40:49 +0200

    added array of images for new arrival section

---
+++ src/assets/images/newArrival.ts
@@ -0,0 +1,23 @@
+import img1 from "./1.webp";
+import img2 from "./2.webp";
+import img3 from "./3.webp";
+import img4 from "./4.webp";
+
+export const imagesArray = [
+  {
+    img: img1,
+    title: "Denim",
+  },
+  {
+    img: img2,
+    title: "Cool Dresses",
+  },
+  {
+    img: img3,
+    title: "Jackets",
+  },
+  {
+    img: img4,
+    title: "T-shirts",
+  },
+];
