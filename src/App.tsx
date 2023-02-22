e729e7f7eb2ea141f23bd373ce5a1d2b156a6922
Yaroslav <6715414@gmail.com>
2023-02-22 11:59:43 +0200

    added Loader and sections(Banner, NewArrival, Footer)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import { ThemeProvider } from "styled-components";
-import { About, Home, Shop } from "./sections";
+import { About, Banner, Footer, Home, NewArrival, Shop } from "./sections";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import { dark } from "./styles/Themes";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 import { AnimatePresence } from "framer-motion";
-import { ScrollTriggerProxy } from "./components";
+import { Loader, ScrollTriggerProxy } from "./components";
 
 function App() {
   const containerRef = useRef<HTMLElement | null>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 3000);
+  }, []);
 
   return (
     <>
@@ -24,12 +31,17 @@ function App() {
           watch={[]}
           containerRef={containerRef}
         >
+          <AnimatePresence>{isLoaded ? null : <Loader />}</AnimatePresence>
           <AnimatePresence>
             <main className="App" ref={containerRef} data-scroll-container>
+              <ScrollTriggerProxy />
+
               <Home />
               <About />
               <Shop />
-              <ScrollTriggerProxy />
+              <Banner />
+              <NewArrival />
+              <Footer />
             </main>
           </AnimatePresence>
         </LocomotiveScrollProvider>
