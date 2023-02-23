ce0116fc06010193037d102cfb19aa9e48a1c62f
Yaroslav <6715414@gmail.com>
2023-02-23 10:58:51 +0200

    added responsive for About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -10,6 +10,14 @@ const Section = styled.section`
   display: flex;
 
   margin: 0 auto;
+
+  @media (max-width: 40em) {
+    width: 90vw;
+  }
+
+  @media (max-width: 30em) {
+    width: 100vw;
+  }
 `;
 
 const Title = styled.h2`
@@ -21,6 +29,16 @@ const Title = styled.h2`
   top: 1rem;
   left: 5%;
   z-index: 3;
+
+  @media (max-width: 64em) {
+    font-size: ${({ theme }) => `calc(${theme.fontBig} - 5vw)`};
+    top: 0;
+    left: 0;
+  }
+
+  @media (max-width: 48em) {
+    font-size: ${({ theme }) => theme.fontxxxl};
+  }
 `;
 
 const Left = styled.div`
@@ -30,6 +48,31 @@ const Left = styled.div`
   position: relative;
   z-index: 3;
   margin-top: 20%;
+
+  @media (max-width: 64em) {
+    width: 80%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) !important;
+
+    margin: 0 auto;
+    padding: 2rem;
+    font-weight: 600;
+
+    backdrop-filter: blur(2px);
+    background-color: ${({ theme }) => `rgba(${theme.textRgba},0.4)`};
+    border-radius: 20px;
+  }
+
+  @media (max-width: 48em) {
+    font-size: ${({ theme }) => theme.fontmd};
+  }
+
+  @media (max-width: 30em) {
+    font-size: ${({ theme }) => theme.fontsm};
+    width: 70%;
+  }
 `;
 
 const Right = styled.div`
@@ -53,6 +96,32 @@ const Right = styled.div`
     width: 40%;
     left: 80%;
     bottom: 10%;
+  }
+
+  @media (max-width: 64em) {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      height: 100vh;
+      object-fit: cover;
+    }
+
+    .small-img-1 {
+      width: 30%;
+      height: auto;
+      left: 5%;
+      bottom: 10%;
+    }
+
+    .small-img-2 {
+      width: 30%;
+      height: auto;
+      left: 60%;
+      bottom: 20%;
+    }
   }
 `;
 
